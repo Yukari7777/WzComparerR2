53f6cdad7943fc2ba0c16f1f00532198ae4ee5f8
Yukari <16526080+Yukari7777@users.noreply.github.com>
2025-09-19 18:13:15 +0900

    Fix crash

--- WzComparerR2/MainForm.cs
+++ WzComparerR2/MainForm.cs
@@ -4324,7 +4324,7 @@ namespace WzComparerR2
                 bool isUpdateRequired = await AutomaticCheckUpdate();
                 if (isUpdateRequired) new FrmUpdater().ShowDialog();
 
-                if (WcR2Config.Default.RecentDocuments[0] != null) //Yukari Auto-Open
+                if (WcR2Config.Default.RecentDocuments.Count > 0)
                 {
                     openWz(WcR2Config.Default.RecentDocuments[0]);
                 }
